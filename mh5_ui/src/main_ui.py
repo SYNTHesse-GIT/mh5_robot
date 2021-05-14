21e5fbe023d7bf3860be38f19447ccb729525e0a
sonelu <alex.sonea@gmail.com>
2021-05-14 19:30:26 +0100

    clanup and code documentation

--- mh5_ui/src/main_ui.py
+++ mh5_ui/src/main_ui.py
@@ -1,12 +1,33 @@
-from snack import SnackScreen
+# Copyright (C) 2020  Alex Sonea
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-# Grid, GridForm, Label, Listbox, Scale, , \
-#                   Textbox, ButtonChoiceWindow
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from snack import SnackScreen, Widget
+
 
 class MainUI():
+    """Main UI class that handles the views and switches between them.
 
-    def __init__(self):
+    The MainUI setups a SnackScreen, determines the size of the avaialable
+    screen and handles the main display loop that processes the hotkeys.
+    An additional hot-key 'q' is provided to quit the loop and close the
+    display.
+    """
+    def __init__(self) -> None:
+        """Initializes the UI. Allocates the SnackScreen, determines the
+        width and height of the screen and initializes the views.
+        """
         # setup the graphics
         self.screen = SnackScreen()
         # for convenience
@@ -17,12 +38,48 @@ class MainUI():
         self.current_view = None
         self.done = False
 
-    def add_view(self, view, hot_key, default_view=False):
+    def add_view(self, view: Widget,
+                       hot_key: str,
+                       default_view: bool = False) -> None:
+        """Adds a view (page) to the dictionay of views. Views are held by
+        their hotkey.
+
+        Parameters
+        ----------
+        view : Widget or subclass
+            The view (page) to be added. The view must be fully constructed
+            and run() must be possible to be executed on that object.
+        hot_key : str
+            The key associated with the view. The main loop will process
+            keys and if they match one of these it will handle the switch
+            to that particular view.
+        default_view : bool, optional
+            Marks this view as the default view whihc means the MainUI will
+            use this to sart displaying the interface when executing run()
+            for the first time. When you add views to the MainUI the last
+            one that uses the ``default_view`` will overwrite the other ones
+            and that will be the one to be used. If no view is defined as
+            ``default_view`` the MainUI will will the first item in the list
+            of hot-keys. Becuase the way the dictionaries work in Python
+            this might not be the first view added. By default False
+        """
         self.views[hot_key] = view
         if default_view:
             self.default_view = hot_key
 
-    def change_view(self, hotkey):
+    def change_view(self, hotkey: str) -> None:
+        """Changes a view to the one specified by the hot-key provided.
+
+        The method will ask the present view to finish() then will popWindow()
+        from the screen. It will asign the view represented in the dictionary
+        by the ``hotkey`` to the ``current_view``, it will ask to setup(),
+        and will setup the hot-keys from that view.
+
+        Parameters
+        ----------
+        hotkey : str
+            The hot-key indentifying that view.
+        """
         if self.current_view:
             self.current_view.finish()
             self.screen.popWindow()
@@ -32,7 +89,21 @@ class MainUI():
             self.current_view.grid.addHotKey(key)
         self.current_view.grid.addHotKey('q')
 
-    def run(self):
+    def run(self) -> None:
+        """Runs the main loop of the UI. It will activate the ``default_view``
+        and then will execute a run() for that view (which for shack means
+        to wait for a key press) then handle the hotkeys by switching the
+        views if they match the ones associated with the views or finish the
+        loop if 'q' was pressed.
+        
+        .. Note:
+
+            The actual refresh of values inside the views is done by the
+            timer that is created by the view itself. See the class View for
+            details.
+        """
+        if not self.default_view:
+            self.default_view = list(self.views.keys())[0]
         self.change_view(self.default_view)
         while not self.done:
             key = self.current_view.run()
--- mh5_ui/src/view_ui.py
+++ mh5_ui/src/view_ui.py
@@ -1,26 +1,61 @@
-from snack import GridForm, Textbox, Scale
+# Copyright (C) 2020  Alex Sonea
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-# Grid, , SnackScreen, Label, Listbox, Scale, , \
-#                   , ButtonChoiceWindow
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from typing import List
+from snack import GridForm, Textbox, Scale, SnackScreen, Widget
 
 
 class View():
     """Base class for a view."""
-    def __init__(self, screen, timer, title):
+    def __init__(self, screen: SnackScreen,
+                       timer: int,
+                       title: str) -> None:
+        """Initializes a new view.
+
+        A view uses a snack.GridForm as a canvas, that is pinned on the screen
+        provided and displays a title.
+
+        The constructor only stores the ``screen``, ``timer`` and ``title``
+        in the internal variables. You need to specifically call setup() to
+        construct the view. setup() will call create_content() that normally
+        needs to be overridden by subclasses to present a specific content.
+
+        Parameters
+        ----------
+        screen : snack.SnackScreen
+            The screen where the view will be positioned.
+        timer : int
+            Refresh time for view in miliseconds. This will trigger the
+            update_content().
+        title : str
+            Title to be presented on the top of the view.
+        """
         self.screen = screen
         self.timer = timer
         self.title = title
 
-    def setup(self):
-        """Must be caleed by the MainUi before starting the view. This
+    def setup(self) -> None:
+        """Builds the view content.
+        
+        Must be called by the MainUi before starting the view. This
         creates all the objects of the UI and initializes them. Sets-up
-        a ``GridForm`` of size 1x1 and calls ``create_content`` to fill
-        the specific content of the view. Subclasses must implement this
-        method. It also revisters the hot keys as are reported by the
-        ``hotkeys`` property that must be subclassed if the view needs to
-        handle keys."""
+        a ``GridForm`` of size 1x1 and calls create_content() to fill
+        the specific content of the view. It also registers the hot 
+        keys as are reported by the ``hotkeys`` property that must be 
+        subclassed if the view needs to handle keys.
+        """
         self.grid = GridForm(self.screen, self.title, 1, 1)
         self.content = self.create_content()
         self.grid.add(self.content, 0, 0)
@@ -29,52 +64,108 @@ class View():
         if self.timer:
             self.grid.setTimer(self.timer)
 
-    def create_content(self):
+    def create_content(self) -> Widget:
         """Should be impelemented in subclasses to produce the desired
-        view output."""
+        view output.
+        
+        Returns
+        -------
+        Widget:
+            A snack.Widget that will be included in the grid. Note that
+            it should be one element only and if you need a more complex
+            structure you need to use a GridForm or other classes to contain
+            and structure the elements. Have a look at the implementation of
+            RobotStatusView, CommsStatusView and JointView.
+        """
         return Textbox(width=20, height=4,
                        text='Default text for the main UI', wrap=1)
 
     @property
-    def hotkeys(self):
+    def hotkeys(self) -> List[str]:
         """Returns the keys this view handles. If implemented by subclasses
         then also ``process_hotkey`` should be implemented."""
         return []
 
-    def update_content(self):
+    def update_content(self) -> None:
         """Handles updates to the content of the view. Normally these are
         triggered by the elpsed timer set up by the ``timer`` property. Should
         be implemented in the subclass according to the desired behaviour."""
         pass
 
-    def process_hotkey(self, key):
-        """Processes the declared hotkeys. Should be implemented in subclass."""
+    def process_hotkey(self, key: str) -> None:
+        """Processes the declared hotkeys. Should be implemented in subclass.
+        
+        Parameters
+        ----------
+        key : str
+            The key to be processed.
+        """
         pass
 
-    def run(self):
-        """Performs a ``run`` of the grid. First calls the ``update_content``
-        to trigger updates to the interface and refreshes the screen. After
-        running a ``grid.run()`` it will ask the ``process_key`` method to
-        process the hotkey pressed (if any) after whicg it returns the hot key
+    def run(self) -> str:
+        """Performs a ``run`` of the grid.
+        
+        First calls the update_content() to trigger updates to the interface 
+        and refresh() on the screen object. After that it runs the run() of
+        the grid object follwied by process_key() method to
+        process the hotkey pressed (if any) after which it returns the hot key
         to the caller program (tipically the MainUI) so that the loop there
-        can process it's own hot keys."""
+        can process it's own hot keys.
+
+        Returns
+        -------
+        str:
+            The key pressed for the caller program to handle if necessary
+        """
         self.update_content()
         self.screen.refresh()
         key = self.grid.run()
         self.process_hotkey(key)
         return key
 
-    def finish(self):
+    def finish(self) -> None:
         """Provides a way for thge view to clear resources before being
-        switched from. For intance views that are displaying information
-        from ROS topics have the change to unsubscribe from the topics here.
+        switched from. For instance views that are displaying information
+        from ROS topics have the chance to unsubscribe from the topics here
+        to save resources.
         """
         pass
 
 
 class NameValueScale():
+    """A display element that includes a name for the object, a value (+ unit
+    of measure if provided) and a Scale (a horizontal bar graph).
+    """
+    def __init__(self, name: str, 
+                       unit: str, 
+                       grid: GridForm, 
+                       row: int, 
+                       widths: List[int], 
+                       min_val: float, 
+                       max_val: float):
+        """Creates a combined display element in one line with a name,
+        a value and a horizontal bar graph.
 
-    def __init__(self, name, unit, grid, row, widths, min_val, max_val):
+        Parameters
+        ----------
+        name : str
+            The name to be shown on the left side of the display.
+        unit : str
+            A string to be shown after the value to denote the unit of measure.
+        grid : GridForm
+            The form where the elements are added to
+        row : int
+            The row number in the form where the elements will be poistioned.
+            All elements are on the same row.
+        widths : List[int]
+            A list of width for the elements (name, value, scale)
+        min_val : float
+            The minium value that the element will display. Needed to calibrate
+            the bar graph.
+        max_val : float
+            The maximum value that the element will display. Needed to calibrate
+            the bar graph.
+        """
         self.unit = unit
         self.name = Textbox(widths[0], 1, name)
         grid.setField(self.name, 0, row)
@@ -86,14 +177,49 @@ class NameValueScale():
         self.scale = Scale(widths[2], int(self.range_val * 100))
         grid.setField(self.scale, 2, row)
 
-    def update_value(self, value, format='4.1f'):
+    def update_value(self, value: float, format: str ='4.1f') -> None:
+        """Updates the content of the elements based on the provided
+        value.
+
+        Parameters
+        ----------
+        value : float
+            The new value to be displayed. This will be reflected in the
+            value field as well as in the bar graph.
+        format : str, optional
+            The format to display the value in the value field, 
+            by default '4.1f'
+        """
         self.value.setText(f'{value:{format}}{self.unit}')
         self.scale.set(int((value - self.min_val) * 100))
 
 
 class NameStatValue():
+    """A display element that includes a name for the object, a staus and
+    an additional (optional can be '') text.
+    """
+    def __init__(self, name: str,
+                       unit: str,
+                       grid: GridForm,
+                       row: int,
+                       widths: List[int]) -> None:
+        """Creates a combined display element in one line with a name,
+        a status (+unit of measure if provided) and a value.
 
-    def __init__(self, name, unit, grid, row, widths):
+        Parameters
+        ----------
+        name : str
+            The name to be shown on the left side of the display.
+        unit : str
+            [A string to be shown after the value to denote the unit of measure.
+        grid : GridForm
+            [description]
+        row : int
+            The row number in the form where the elements will be poistioned.
+            All elements are on the same row.
+        widths : List[int]
+            A list of width for the elements (name, status, value)
+        """
         self.unit = unit
         self.name = Textbox(widths[0], 1, name)
         grid.setField(self.name, 0, row)
@@ -102,6 +228,16 @@ class NameStatValue():
         self.value = Textbox(widths[2], 1, '')
         grid.setField(self.value, 2, row)
 
-    def update_value(self, stat, value=''):
+    def update_value(self, stat: str, value: str ='') -> None:
+        """Updates the content of the elements based on the provided
+        value.
+
+        Parameters
+        ----------
+        stat : str
+            A string showing the status of the element.
+        value : str, optional
+            An additional valuee to be shown after the status, by default ''.
+        """
         self.stat.setText(f'{stat:>4s}{self.unit}')
         self.value.setText(value)
